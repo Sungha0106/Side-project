a9bf428965785f661aef7d42f340407e5cbcb994
Sungha0106 <144648898+Sungha0106@users.noreply.github.com>
2024-04-01 22:57:02 +0800

    IC Contest

--- 2016 IC Contest Local Binary Patterns/LBP.v
+++ 2016 IC Contest Local Binary Patterns/LBP.v
@@ -8,193 +8,360 @@ output reg [13:0]   gray_addr;
 output reg          gray_req;
 output reg [13:0]   lbp_addr;
 output reg          lbp_valid;
-output reg [7:0]    lbp_data; 
+output reg [7:0]    lbp_data;
 output reg          finish;
 //====================================================================
-
-reg[3:0] counter ;
-reg[10:0] edge_counter ;
-reg[13:0] gray_0 ; // 儲存中心gray_addr
-reg[7:0] gray_data0, gray_data1, gray_data2, gray_data3, gray_data4, gray_data5, gray_data6, gray_data7, gray_data8 ; // 儲存gray_data ;
-reg[7:0] LBP_0, LBP_1, LBP_2, LBP_3, LBP_4, LBP_5, LBP_6, LBP_7 ; // 儲存各LBP計算值
-
-always @(posedge clk or posedge reset) // 計算9次
+reg[1:0] state, next_state;
+parameter[1:0] IDLE = 2'b00, DATA_PASS_LBP = 2'b01, TRANSFORM = 2'b10, DATA_STORE_HOST = 2'b11;
+reg[7:0] lbp_mem0, lbp_mem1, lbp_mem2, lbp_mem3, lbp_mem4, lbp_mem5, lbp_mem6, lbp_mem7, lbp_mem8;
+reg[3:0] counter1;
+//====================================================================
+//  state 轉換  //
+always @(*)
 begin
-    if (reset)
-    begin
-        counter <= 4'd0 ;
-    end 
-    else if (counter > 4'd8)
-    begin
-        counter <= 4'd0 ;
-    end
-    else
-    begin
-        counter <= counter + 1'd1 ;
-    end
-end
-
-always @(posedge clk or posedge reset) // 邊緣偵測
-begin
-    if (reset)
-    begin
-        edge_counter <= 11'd0 ;
-    end
-    else if (edge_counter == 11'd1259)
-    begin
-        edge_counter <= 11'd0 ;
-    end
-    else
-    begin
-        edge_counter <= edge_counter + 1'd1 ;
-    end
-end
-
-
-always @(posedge clk or posedge reset) // gray_req 輸出
-begin
-    if (reset)
-    begin
-        gray_req <= 1'd0 ; 
-    end 
-    else begin
-        gray_req <= (gray_ready == 1'd1 && counter == 4'd9)?1'd0 : 1'd1 ;
-    end
-end
-
-always @(posedge clk or posedge reset) // 設定gray_addr位置 儲存原始addr
-begin
-    if (reset) 
-    begin
-        gray_addr <= 14'd0 ;
-        gray_0 <= 14'd129 ;
-    end 
-    else begin
-        if (gray_ready == 1'd1)
+    case(state)
+        IDLE :
         begin
-            if (edge_counter == 11'd1259)
+            if(finish)
+                next_state = IDLE;
+            else
             begin
-                gray_0 <= gray_0 + 2'd3 ;
+                if(gray_req)
+                    next_state = DATA_PASS_LBP;
+                else
+                    next_state = IDLE;
             end
-            else begin
-                case (counter)
-                    4'd9: begin
-                         gray_addr <= gray_0 + 14'd129 ;
-                         gray_0 <= gray_0 + 1'd1 ;
-                    end
-                    4'd8: gray_addr <= gray_0 + 14'd129 ;
-                    4'd7: gray_addr <= gray_0 + 14'd128 ;
-                    4'd6: gray_addr <= gray_0 + 14'd127 ;
-                    4'd5: gray_addr <= gray_0 + 14'd1 ;
-                    4'd4: gray_addr <= gray_0 - 14'd1 ;
-                    4'd3: gray_addr <= gray_0 - 14'd127 ;
-                    4'd2: gray_addr <= gray_0 - 14'd128 ;
-                    4'd1: gray_addr <= gray_0 - 14'd129 ;
-                    4'd0: gray_addr <= gray_0 ;
-                    default: gray_addr <= gray_addr ; 
-                endcase
-            end
-        end 
-       else begin
-          gray_addr <= gray_addr ;
-       end
-    end
-end
+        end
 
-always @(*) // 儲存gray_data
-begin
-     begin
-        case (counter)
-           4'd9: gray_data8 = gray_data ;
-           4'd8: gray_data7 = gray_data ;
-           4'd7: gray_data6 = gray_data ;
-           4'd6: gray_data5 = gray_data ;
-           4'd5: gray_data4 = gray_data ;
-           4'd4: gray_data3 = gray_data ;
-           4'd3: gray_data2 = gray_data ;
-           4'd2: gray_data1 = gray_data ;
-           4'd1: gray_data0 = gray_data ;
-           default: gray_data0 = gray_data ; 
-        endcase
-    end
-end
-
-always @(posedge clk or posedge reset) // lbp_addr 位置
-begin
-    if (reset)
-    begin
-        lbp_addr <= 14'd0 ;
-    end
-    else begin
-        lbp_addr <= (gray_ready == 1'd1 && counter == 4'd9)? gray_0 : lbp_addr ;
-    end
-end
-
-always @(*) // LBP計算
-begin
-     begin
-        case (counter)
-           4'd9: LBP_7 = (gray_data0 <= gray_data8)? 8'd1<<8'd7 : 8'd0 ;
-           4'd8: LBP_6 = (gray_data0 <= gray_data7)? 8'd1<<8'd6 : 8'd0 ;
-           4'd7: LBP_5 = (gray_data0 <= gray_data6)? 8'd1<<8'd5 : 8'd0 ;
-           4'd6: LBP_4 = (gray_data0 <= gray_data5)? 8'd1<<8'd4 : 8'd0 ;
-           4'd5: LBP_3 = (gray_data0 <= gray_data4)? 8'd1<<8'd3 : 8'd0 ;
-           4'd4: LBP_2 = (gray_data0 <= gray_data3)? 8'd1<<8'd2 : 8'd0 ;
-           4'd3: LBP_1 = (gray_data0 <= gray_data2)? 8'd1<<8'd1 : 8'd0 ;
-           4'd2: LBP_0 = (gray_data0 <= gray_data1)? 8'd1 : 8'd0 ; 
-           default: LBP_0 = LBP_0 ; 
-        endcase
-    end
-end
-
-always @(posedge clk or posedge reset) // lbp_data輸出
-begin
-    if (reset)
-    begin
-        lbp_data <= 8'd0 ;
-    end 
-    else begin
-       case (counter)
-           4'd9: lbp_data <= LBP_0 + LBP_1 + LBP_2 + LBP_3 + LBP_4 + LBP_5 + LBP_6 + LBP_7 ; 
-           4'd8: lbp_data <= LBP_0 + LBP_1 + LBP_2 + LBP_3 + LBP_4 + LBP_5 + LBP_6 ; 
-           4'd7: lbp_data <= LBP_0 + LBP_1 + LBP_2 + LBP_3 + LBP_4 + LBP_5 ;
-           4'd6: lbp_data <= LBP_0 + LBP_1 + LBP_2 + LBP_3 + LBP_4 ;
-           4'd5: lbp_data <= LBP_0 + LBP_1 + LBP_2 + LBP_3 ;
-           4'd4: lbp_data <= LBP_0 + LBP_1 + LBP_2 ;
-           4'd3: lbp_data <= LBP_0 + LBP_1 ;
-           4'd2: lbp_data <= LBP_0 ;
-           4'd1: lbp_data <= 8'd0 ;
-           4'd0: lbp_data <= 8'd0 ;
-           default: lbp_data <= lbp_data ; 
-       endcase
-    end
-end
-
-always @(posedge clk or posedge reset) // lbp_valid 輸出
-begin
-    if (reset)
-    begin
-        lbp_valid <= 1'd0 ; 
-    end 
-    else begin
-        lbp_valid <= (gray_ready == 1'd1 && counter == 4'd9)? 1'd1 : 1'd0 ;
-    end
-end
-
-always @(posedge clk or posedge reset) // finish 輸出
-begin
-    if (reset)
-    begin
-        finish <= 1'd0 ;
-    end 
-    else begin
-        if (gray_addr == 14'd16383)
+        DATA_PASS_LBP :
         begin
-            finish <= 1'd1 ;
+            if(gray_req)
+                next_state = DATA_PASS_LBP;
+            else
+                next_state = TRANSFORM;
         end
-        else begin
-            finish <= 1'd0 ;
+
+        TRANSFORM :
+            next_state = DATA_STORE_HOST;
+
+        DATA_STORE_HOST :
+        begin
+            if(lbp_addr==14'd16254)
+                next_state = IDLE;
+            else
+            begin
+                if(gray_req)
+                    next_state = DATA_PASS_LBP;
+                else
+                    next_state = IDLE;
+            end
         end
+
+
+    endcase
+end
+
+//  state 儲存  //
+always@(posedge clk or posedge reset)
+begin
+    if(reset)
+        state <= IDLE;
+    else
+        state <= next_state;
+end
+//  counter1 數 HOST 送到哪  //
+always @(posedge clk or posedge reset)
+begin
+    if(reset)
+        counter1 <= 4'd0;
+    else
+    begin
+        if(state == DATA_PASS_LBP)
+        begin
+            if(counter1<4'd9)
+                counter1 <= counter1 + 4'd1;
+            else
+                counter1 <= 4'd0;
+        end
+        else
+            counter1 <= 4'd0;
+    end
+end
+//  gray_req 信號線  //
+always @(posedge clk or posedge reset)
+begin
+    if(reset)
+        gray_req <= 1'b0;
+    else
+    begin
+        if(state==IDLE)
+        begin
+            if(gray_ready)
+                gray_req <= 1'b1;
+            else
+                gray_req <= 1'b0;
+        end
+        else if(state == DATA_PASS_LBP)
+        begin
+            if(counter1==4'd8)
+                gray_req <= 1'b0;
+            else
+                gray_req <= gray_req;
+        end
+        else if(state == DATA_STORE_HOST && finish == 1'd0)
+            gray_req <= 1'd1;
+        else
+            gray_req <= gray_req;
+    end
+end
+//  給 address 後讀 data  //
+always @(posedge clk or posedge reset)
+begin
+    if(reset)
+    begin
+        gray_addr <= 14'd129;
+        lbp_mem4 <= 8'd0;
+    end
+    else
+    begin
+        if(state == DATA_PASS_LBP)
+        begin
+            case(counter1)
+                4'd0 :
+                begin
+                    lbp_mem4 <= gray_data;            //129
+                    gray_addr <= gray_addr - 14'd129; //0
+                end
+                4'd1 :
+                    gray_addr <= gray_addr + 14'd1;   //1
+                4'd2 :
+                    gray_addr <= gray_addr + 14'd1;   //2
+                4'd3 :
+                    gray_addr <= gray_addr + 14'd126; //128
+                4'd4 :
+                    gray_addr <= gray_addr + 14'd2 ;  //130
+                4'd5 :
+                    gray_addr <= gray_addr + 14'd126; //256
+                4'd6 :
+                    gray_addr <= gray_addr + 14'd1;   //257
+                4'd7 :
+                    gray_addr <= gray_addr + 14'd1; //258
+                4'd8 :
+                begin
+                    if(gray_addr==14'd16383)
+                        gray_addr <= 14'd129;
+                    else
+                    begin
+                        if(&gray_addr[6:0])
+                            gray_addr <= gray_addr - 14'd126;
+                        else
+                            gray_addr <= gray_addr - 14'd128;
+                    end
+                end
+                default :
+                    gray_addr <= gray_addr;
+            endcase
+        end
+        else
+            gray_addr <= gray_addr;
+    end
+end
+//  進行 LBP 運算  //
+//====================================================================
+always @(posedge clk or posedge reset)
+begin
+    if(reset)
+        lbp_mem0 <= 8'd0;
+    else if(state == DATA_PASS_LBP && counter1 == 4'd1)
+        lbp_mem0 <= gray_data;
+    else if(state == TRANSFORM)
+    begin
+        if(lbp_mem0>=lbp_mem4)
+            lbp_mem0 <= 8'd1;
+        else
+            lbp_mem0 <= 8'd0;
+    end
+    else
+        lbp_mem0 <= lbp_mem0;
+end
+
+always @(posedge clk or posedge reset)
+begin
+    if(reset)
+        lbp_mem1 <= 8'd0;
+    else if(state == DATA_PASS_LBP && counter1 == 4'd2)
+        lbp_mem1 <= gray_data;
+    else if(state == TRANSFORM)
+    begin
+        if(lbp_mem1>=lbp_mem4)
+            lbp_mem1 <= 8'd1;
+        else
+            lbp_mem1 <= 8'd0;
+    end
+    else
+        lbp_mem1 <= lbp_mem1;
+end
+
+always @(posedge clk or posedge reset)
+begin
+    if(reset)
+        lbp_mem2 <= 8'd0;
+    else if(state == DATA_PASS_LBP && counter1 == 4'd3)
+        lbp_mem2 <= gray_data;
+    else if(state == TRANSFORM)
+    begin
+        if(lbp_mem2>=lbp_mem4)
+            lbp_mem2 <= 8'd1;
+        else
+            lbp_mem2 <= 8'd0;
+    end
+    else
+        lbp_mem2 <= lbp_mem2;
+end
+
+always @(posedge clk or posedge reset)
+begin
+    if(reset)
+        lbp_mem3 <= 8'd0;
+    else if(state == DATA_PASS_LBP && counter1 == 4'd4)
+        lbp_mem3 <= gray_data;
+    else if(state == TRANSFORM)
+    begin
+        if(lbp_mem3>=lbp_mem4)
+            lbp_mem3 <= 8'd1;
+        else
+            lbp_mem3 <= 8'd0;
+    end
+    else
+        lbp_mem3 <= lbp_mem3;
+end
+
+always @(posedge clk or posedge reset)
+begin
+    if(reset)
+        lbp_mem5 <= 8'd0;
+    else if(state == DATA_PASS_LBP && counter1 == 4'd5)
+        lbp_mem5 <= gray_data;
+    else if(state == TRANSFORM)
+    begin
+        if(lbp_mem5>=lbp_mem4)
+            lbp_mem5 <= 8'd1;
+        else
+            lbp_mem5 <= 8'd0;
+    end
+    else
+        lbp_mem5 <= lbp_mem5;
+end
+
+always @(posedge clk or posedge reset)
+begin
+    if(reset)
+        lbp_mem6 <= 8'd0;
+    else if(state == DATA_PASS_LBP && counter1 == 4'd6)
+        lbp_mem6 <= gray_data;
+    else if(state == TRANSFORM)
+    begin
+        if(lbp_mem6>=lbp_mem4)
+            lbp_mem6 <= 8'd1;
+        else
+            lbp_mem6 <= 8'd0;
+    end
+    else
+        lbp_mem6 <= lbp_mem6;
+end
+
+always @(posedge clk or posedge reset)
+begin
+    if(reset)
+        lbp_mem7 <= 8'd0;
+    else if(state == DATA_PASS_LBP && counter1 == 4'd7)
+        lbp_mem7 <= gray_data;
+    else if(state == TRANSFORM)
+    begin
+        if(lbp_mem7>=lbp_mem4)
+            lbp_mem7 <= 8'd1;
+        else
+            lbp_mem7 <= 8'd0;
+    end
+    else
+        lbp_mem7 <= lbp_mem7;
+end
+
+always @(posedge clk or posedge reset)
+begin
+    if(reset)
+        lbp_mem8 <= 8'd0;
+    else if(state == DATA_PASS_LBP && counter1 == 4'd8)
+        lbp_mem8 <= gray_data;
+    else if(state == TRANSFORM)
+    begin
+        if(lbp_mem8>=lbp_mem4)
+            lbp_mem8 <= 8'd1;
+        else
+            lbp_mem8 <= 8'd0;
+    end
+    else
+        lbp_mem8 <= lbp_mem8;
+end
+
+always @(posedge clk or posedge reset)
+begin
+    if(reset)
+        lbp_data <= 8'd0;
+    else
+    begin
+        if(state == DATA_STORE_HOST)
+            lbp_data <= {lbp_mem8[0],lbp_mem7[0],lbp_mem6[0],lbp_mem5[0],lbp_mem3[0],lbp_mem2[0],lbp_mem1[0],lbp_mem0[0]};
+        else
+            lbp_data <= lbp_data;
+    end
+end
+//====================================================================
+//  lbp_valid 信號線  //
+always @(posedge clk or posedge reset)
+begin
+    if(reset)
+        lbp_valid <= 1'd0;
+    else
+    begin
+        if(state==DATA_STORE_HOST)
+            lbp_valid <= 1'd1;
+        else
+            lbp_valid <= 1'd0;
+    end
+end
+
+//  address 傳給 lbp_addr  //
+always @(posedge clk or posedge reset)
+begin
+    if(reset)
+        lbp_addr <= 14'd0;
+    else
+    begin
+        if(state == DATA_PASS_LBP)
+        begin
+            if(counter1==4'd0)
+                lbp_addr <= gray_addr;
+            else
+                lbp_addr <= lbp_addr;
+        end
+        else
+            lbp_addr <= lbp_addr;
+    end
+end
+
+
+//  finish 信號線  //
+always @(posedge clk or posedge reset)
+begin
+    if(reset)
+        finish <= 1'd0;
+    else
+    begin
+        if(state==IDLE && lbp_addr==14'd16254)
+            finish <= 1'd1;
+        else
+            finish <= 1'd0;
     end
 end
 
